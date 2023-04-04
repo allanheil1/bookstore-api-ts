4437cc8f7fa604910137402409839849b2ee568a
allanheil1 <allanheil1@gmail.com>
2023-04-04 19:05:00 -0300

    js project

---
+++ src/controllers/userControllers.ts
@@ -0,0 +1,27 @@
+import userServices from "../services/userServices.js";
+import { Request, Response, NextFunction } from 'express';
+
+async function create(req: Request, res: Response, next: NextFunction) {
+  const { name, email, password } = req.body;
+  try {
+    await userServices.create({ name, email, password });
+    return res.sendStatus(201);
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function signin(req: Request, res: Response, next: NextFunction) {
+  const { email, password } = req.body;
+  try {
+    const token = await userServices.signin({ email, password });
+    return res.send({ token });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export default {
+  create,
+  signin,
+};
